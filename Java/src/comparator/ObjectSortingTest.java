4e1826cda706ce1969bf36c816fcca8e8da73081
deepak.kumar <deepak.kumar@getzephyr.com>
2018-10-03 14:29:43 +0530

    sorting objects using comparable and comparator

---
+++ Java/src/comparator/ObjectSortingTest.java
@@ -0,0 +1,197 @@
+package comparator;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ObjectSortingTest {
+
+	public static String NAME = "name";
+	public static String ID = "id";
+	public static String AGE = "age";
+	public static String SALARY = "salary";
+
+	@Test
+	public void comparableTest_ListSorting() {
+		List<Person> persons = getPersonList();
+
+		List<Integer> sortedIds = new ArrayList();
+		for (Person p : persons) {
+			sortedIds.add(p.getId());
+		}
+		Collections.sort(sortedIds);
+
+		Collections.sort(persons);
+
+		int index = 0;
+
+		for (Person p : persons) {
+			assertTrue(sortedIds.get(index++) - p.getId() == 0);
+		}
+	}
+
+	@Test
+	public void comparableTest_ArraySorting() {
+		Person[] persons = getPersonArray();
+
+		List<Integer> sortedIds = new ArrayList();
+		for (Person p : persons) {
+			sortedIds.add(p.getId());
+		}
+		Collections.sort(sortedIds);
+
+		Arrays.sort(persons);
+
+		int index = 0;
+		for (Person p : persons) {
+			assertTrue(sortedIds.get(index++) - p.getId() == 0);
+		}
+	}
+
+	@Test
+	public void ageComparatorTest_ListSorting() {
+		List<Person> persons = getPersonList();
+
+		List<Integer> sortedAges = new ArrayList();
+		for (Person p : persons) {
+			sortedAges.add(p.getAge());
+		}
+		Collections.sort(sortedAges);
+
+		Collections.sort(persons, Person.ageComparator);
+
+		int index = 0;
+
+		for (Person p : persons) {
+			assertTrue(sortedAges.get(index++) - p.getAge() == 0);
+		}
+	}
+
+	@Test
+	public void salaryComparatorTest_ListSorting() {
+		List<Person> persons = getPersonList();
+
+		List<Integer> sortedSalaries = new ArrayList();
+		for (Person p : persons) {
+			sortedSalaries.add((int) p.getSalary());
+		}
+		Collections.sort(sortedSalaries);
+
+		Collections.sort(persons, Person.salaryComparator);
+
+		int index = 0;
+
+		for (Person p : persons) {
+			assertTrue(sortedSalaries.get(index++) - p.getSalary() == 0);
+		}
+	}
+
+	@Test
+	public void nameComparatorTest_ListSorting() {
+		List<Person> persons = getPersonList();
+
+		List<String> sortedNames = new ArrayList();
+		for (Person p : persons) {
+			sortedNames.add(p.getName());
+		}
+		Collections.sort(sortedNames);
+
+		Collections.sort(persons, Person.nameComparator);
+
+		int index = 0;
+
+		for (Person p : persons) {
+			assertTrue(sortedNames.get(index++).equals(p.getName()));
+		}
+	}
+
+	@Test
+	public void ageComparatorTest_ArraySorting() {
+		Person[] persons = getPersonArray();
+
+		List<Integer> sortedAges = new ArrayList();
+		for (Person p : persons) {
+			sortedAges.add(p.getAge());
+		}
+		Collections.sort(sortedAges);
+
+		Arrays.sort(persons, Person.ageComparator);
+
+		int index = 0;
+
+		for (Person p : persons) {
+			assertTrue(sortedAges.get(index++) - p.getAge() == 0);
+		}
+	}
+
+	@Test
+	public void salaryComparatorTest_ArraySorting() {
+		Person[] persons = getPersonArray();
+
+		List<Integer> sortedSalaries = new ArrayList();
+		for (Person p : persons) {
+			sortedSalaries.add((int) p.getSalary());
+		}
+		Collections.sort(sortedSalaries);
+
+		Arrays.sort(persons, Person.salaryComparator);
+
+		int index = 0;
+
+		for (Person p : persons) {
+			assertTrue(sortedSalaries.get(index++) - p.getSalary() == 0);
+		}
+	}
+
+	@Test
+	public void nameComparatorTest_ArraySorting() {
+		Person[] persons = getPersonArray();
+
+		List<String> sortedNames = new ArrayList();
+		for (Person p : persons) {
+			sortedNames.add(p.getName());
+		}
+		Collections.sort(sortedNames);
+
+		Arrays.sort(persons, Person.nameComparator);
+
+		int index = 0;
+
+		for (Person p : persons) {
+			assertTrue(sortedNames.get(index++).equals(p.getName()));
+		}
+	}
+
+	private List<Person> getPersonList() {
+		Person person1 = new Person(1, "Pankaj", 32, 50000);
+		Person person2 = new Person(5, "Lisa", 35, 5000);
+		Person person3 = new Person(10, "Mikey", 25, 10000);
+		Person person4 = new Person(20, "Arun", 29, 20000);
+
+		List<Person> persons = new ArrayList<>();
+		persons.add(person3);
+		persons.add(person4);
+		persons.add(person1);
+		persons.add(person2);
+		return persons;
+	}
+
+	private Person[] getPersonArray() {
+		Person person1 = new Person(1, "Pankaj", 32, 50000);
+		Person person2 = new Person(5, "Lisa", 35, 5000);
+		Person person3 = new Person(10, "Mikey", 25, 10000);
+		Person person4 = new Person(20, "Arun", 29, 20000);
+
+		Person[] persons = new Person[4];
+		persons[0] = person3;
+		persons[1] = person4;
+		persons[2] = person1;
+		persons[3] = person2;
+		return persons;
+	}
+}
---
+++ Java/src/comparator/Person.java
@@ -0,0 +1,93 @@
+package comparator;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Person implements Comparable<Person> {
+
+	private int id;
+	private String name;
+	private int age;
+	private long salary;
+
+	@Override
+	public int compareTo(Person o) {
+		return (int) (this.id - o.id);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + ", salary=" + salary + "]";
+	}
+
+	public Person(int id, String name, int age, long salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public Person() {
+	}
+
+	public static void printPersonList(List<Person> persons) {
+		for (Person p : persons) {
+			System.out.println(p.id + " " + p.name + " " + p.age + " " + p.salary);
+		}
+	}
+
+	public static Comparator<Person> salaryComparator = new Comparator<Person>() {
+
+		@Override
+		public int compare(Person o1, Person o2) {
+			return (int) (o1.getSalary() - o2.getSalary());
+		}
+
+	};
+	public static Comparator<Person> ageComparator = new Comparator<Person>() {
+		@Override
+		public int compare(Person o1, Person o2) {
+			return (int) (o1.getAge() - o2.getAge());
+		}
+	};
+	public static Comparator<Person> nameComparator = new Comparator<Person>() {
+		@Override
+		public int compare(Person o1, Person o2) {
+			return o1.getName().compareTo(o2.getName());
+		}
+	};
+
+}
